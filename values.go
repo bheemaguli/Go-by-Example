49a85dd18f51903bffbb804409796286aafc0731
Sumanth Kashyap <sumanth.bhi@gmail.com>
2024-07-02 15:16:36 +0530

    Values

---
+++ values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang") // string concat
+
+	fmt.Println("1+1 = ", 1+1)         // Integer Addition
+	fmt.Println("7.0/3.0 = ", 7.0/3.0) // Float division
+
+	// Boolean operation
+	fmt.Println(true && false) // And
+	fmt.Println(true || false) // Or
+	fmt.Println(!true)         // Not
+}
